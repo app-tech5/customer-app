305e691c6b5f3ab551fb61ff178d6e427b90dc5a
nass <cabliveer@gmail.com>
2026-03-12 14:30:47 +0100

    Refactor GroupFoodHeader component to remove unused imports, enhancing code clarity and reducing bundle size.

--- components/GroupFoodHeader.js
+++ components/GroupFoodHeader.js
@@ -1,7 +1,6 @@
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, FlatList, StatusBar} from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, FlatList} from 'react-native'
 import React, { useState } from 'react'
 import { groupFoods } from '../data'
-import { BottomSheetFlatList } from '@gorhom/bottom-sheet'
 import { grey1 } from '../global'
 
 export default function GroupFoodHeader({foodsRef}) {
