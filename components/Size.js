713c82b9a159f174058e98b1da6d19b0962c46d5
nass <cabliveer@gmail.com>
2026-03-13 20:41:08 +0100

    Refactor Size component to improve code clarity by removing unnecessary comments and whitespace, enhancing readability while maintaining functionality.

--- components/Size.js
+++ components/Size.js
@@ -1,12 +1,12 @@
 import { useState } from "react"
 import { Text, View, StyleSheet } from "react-native"
 import { CheckBox } from "react-native-elements"
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { currency, language } from "../global"
 
 const Size = ({ food, restaurant }) => {
     const dispatch = useDispatch();
-    // const items = useSelector((state)=>state.cartReducer).filter(item => item.restaurantName === restaurant.name)
+    
     const [checked, setChecked] = useState(new Array(Object.keys(food.size).length).fill(false))
 
     return Object.keys(food.size).map((key, index) => {
@@ -18,7 +18,7 @@ const Size = ({ food, restaurant }) => {
                     uncheckedIcon='circle-o'
                     checkedIcon='dot-circle-o'
                     onPress={() => {
-                        // if(!items.some(item => item.name === food.name))
+                        
                         dispatch({
                             type: 'UPDATE_FROM_CARD',
                             payload: {
@@ -50,7 +50,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        // borderWidth: 2,
+        
         marginHorizontal: 10,
         paddingRight: 10,
         backgroundColor: "white"
