fc8c234cd4059aab29c972a633a3f473f5caf4fe
nass <cabliveer@gmail.com>
2026-03-15 15:11:00 +0100

    Enhance localization in FormElement.js by replacing static text with i18n translations, improving internationalization support and user experience.

--- screens/FormElement.js
+++ screens/FormElement.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import i18n from '../i18n'
 
 export default function FormElement() {
   return (
     <View>
-      <Text>FormElement</Text>
+      <Text>{i18n.t('common.formElement')}</Text>
     </View>
   )
 }
