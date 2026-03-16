87e2d26c7e9a1a559217b415c73bdbb224e704ac
nass <cabliveer@gmail.com>
2026-03-16 21:47:23 +0100

    Remove Storybook integration and related files to streamline the project. Updated App.js to eliminate Storybook conditional rendering and cleaned up package.json by removing Storybook dependencies and scripts. This refactor enhances code clarity and maintainability.

--- .storybook/main.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  stories: ['../stories-web/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
-};
-
--- storybook/rn-addons.js
+++
@@ -1,3 +0,0 @@
-// Addon managers disabled for now to avoid missing package errors.
-// On-device actions/controls are already wired via @storybook/react-native defaults.
-
